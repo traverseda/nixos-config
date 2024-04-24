87a4f01a07ceb94ee793e6b72aa1408d5d0eaed3
Alex Davies <traverse.da@gmail.com>
2024-04-24 09:55:10 -0300

    aider: Fix nixos/configuration.nix to accept `hostname` as an argument.
    # Aider chat conversation:
    
    USER: I ran this command:
    
     nixos-rebuild build-vm --flake ./#athame
    
    And got this output:
    
    building the system configuration...
    warning: Git tree '/home/traverseda/Code/nixos-config' is dirty
    error:
           … while calling the 'derivationStrict' builtin
    
             at /builtin/derivation.nix:9:12: (source not available)
    
           … while evaluating derivation 'nixos-vm'
             whose name attribute is located at /nix/store/0yzh1r9mhsr4vp5w96y3i0jgz3m2qzv6-source/pkgs/stdenv/generic/make-derivation.nix:303:7
    
           … while evaluating attribute 'buildCommand' of derivation 'nixos-vm'
    
             at /nix/store/0yzh1r9mhsr4vp5w96y3i0jgz3m2qzv6-source/pkgs/build-support/trivial-builders/default.nix:87:14:
    
               86|       enableParallelBuilding = true;
               87|       inherit buildCommand name;
                 |              ^
               88|       passAsFile = [ "buildCommand" ]
    
           (stack trace truncated; use '--show-trace' to show the full trace)
    
           error: attribute 'hostname' missing
    
           at /nix/store/ds44l4g1yz0dd3ssqhlpv4r6wwh8p6r0-source/nixos/configuration.nix:77:25:
    
               76|   # TODO: Set your hostname
               77|   networking.hostName = inputs.hostname;
                 |                         ^
               78|
    
    ASSISTANT: The error message indicates that the attribute 'hostname' is missing. This is likely because the `inputs` in `nixos/configuration.nix` does not contain a `hostname` attribute.
    
    The `hostname` attribute is defined in `flake.nix` under `nixosConfigurations` for each system (like `athame` and `metatron`). However, this `hostname` attribute is not being passed to `nixos/configuration.nix`.
    
    To fix this, we need to modify `nixos/configuration.nix` to accept `hostname` as an argument.
    
    Here is the *SEARCH/REPLACE* block:
    
    nixos/configuration.nix
    ```nix
    <<<<<<< SEARCH
    {
      inputs,
      outputs,
      lib,
      config,
      pkgs,
      ...
    }: {
    =======
    { inputs, outputs, lib, config, pkgs, hostname, ... }: {
    >>>>>>> REPLACE
    ```
    
    This change will allow `nixos/configuration.nix` to accept `hostname` as an argument. Now, when you run the command `nixos-rebuild build-vm --flake ./#athame`, the `hostname` attribute from `flake.nix` will be passed to `nixos/configuration.nix`, and the error should be resolved.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,13 +1,6 @@
 # This is your system's configuration file.
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
-{
-  inputs,
-  outputs,
-  lib,
-  config,
-  pkgs,
-  ...
-}: {
+{ inputs, outputs, lib, config, pkgs, hostname, ... }: {
   # You can import other NixOS modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/nixos):
