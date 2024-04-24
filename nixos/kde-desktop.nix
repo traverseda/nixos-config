069caf37bf1f516154598d5dd9a83c7313ecb29a
Alex Davies <traverse.da@gmail.com>
2024-04-24 09:48:02 -0300

    aider: Create a basic KDE desktop configuration in `nixos/kde-desktop.nix`.
    # Aider chat conversation:
    
    USER: Let's create the kde-desktop.nix
    
    ASSISTANT: Sure, let's create a new file `nixos/kde-desktop.nix` with a basic KDE desktop configuration.
    
    Here is the *SEARCH/REPLACE* block:
    
    nixos/kde-desktop.nix
    ```nix
    <<<<<<< SEARCH
    =======
    { config, pkgs, lib, ... }:
    
    {
      # Enable the KDE Desktop Environment.
      services.xserver.displayManager.sddm.enable = true;
      services.xserver.desktopManager.plasma5.enable = true;
    
      # Define a user account. Don't forget to set a password with ‘passwd’.
      users.users.jane = {
        isNormalUser = true;
        extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
      };
    
      # This value determines the NixOS release from which the default
      # settings for stateful data, like file locations and database versions
      # on your system were taken. It‘s perfectly fine and recommended to leave
      # this value at the release version of the first install of this system.
      # Before changing this value read the documentation for this option
      # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
      system.stateVersion = "23.05"; # Did you read the comment?
    }
    >>>>>>> REPLACE
    ```
    
    This is a basic KDE desktop configuration. You can customize it according to your needs.

---
+++ nixos/kde-desktop.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, lib, ... }:
+
+{
+  # Enable the KDE Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.jane = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+}
