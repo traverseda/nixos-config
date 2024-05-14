c3ebc2312b1ededaf8df45514f41ef9606f165af
Alex Davies <traverse.da@gmail.com>
2024-05-14 07:30:11 -0300

    Add nextcloud to installed apps

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -114,6 +114,7 @@
       PermitRootLogin = "no";
       # Use keys only. Remove if you want to SSH using password (not recommended)
       PasswordAuthentication = true;
+      AllowUsers = [ "traverseda" ];
     };
   };
 
