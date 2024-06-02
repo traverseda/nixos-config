f7cc91e85059ad416756df478a08b2dc0af04ee9
Alex Davies <traverse.da@gmail.com>
2024-06-02 12:25:41 -0300

    Added vscode and htop

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -87,6 +87,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
      pkgs.mosh
+     pkgs.htop
      pkgs.git
      pkgs.usbutils
      pkgs.pciutils
