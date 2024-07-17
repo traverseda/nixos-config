88395bdce5a46e090787c79e55aca28289000114
Alex Davies <traverse.da@gmail.com>
2024-07-16 22:21:26 -0300

    Provision adrial

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -57,6 +57,10 @@
   # To make nix3 commands consistent with your flake
   nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
 
+  services.udev.packages = [
+    pkgs.android-udev-rules
+  ];
+
   # This will additionally add your inputs to the system's legacy channels
   # Making legacy nix commands consistent as well, awesome!
   nix.nixPath = ["/etc/nix/path"];
