0c27e40895230a5a03d593a92e38879006be6637
Alex Davies <traverse.da@gmail.com>
2024-11-24 10:00:33 -0400

    Trying to figure out lightdm

--- nixos/kde-desktop.nix
+++ nixos/kde-desktop.nix
@@ -27,6 +27,8 @@
   services.desktopManager.plasma6.enable = true;
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
+
+
   services.displayManager.defaultSession = "plasma";
   services.displayManager = {
     autoLogin.enable = lib.mkDefault false;
