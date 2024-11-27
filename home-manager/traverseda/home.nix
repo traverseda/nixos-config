aca04981713aa88a98a0a90ff7ed33585bd2ec38
Alex Davies <traverse.da@gmail.com>
2024-11-27 11:47:01 -0400

    chore: Remove unused formatting script from home.nix

--- home-manager/traverseda/home.nix
+++ home-manager/traverseda/home.nix
@@ -170,16 +170,3 @@
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.05";
 }
-#!/usr/bin/env bash
-
-# This script formats all files in a given directory using Neovim's LSP
-
-set -e
-
-DIRECTORY=${1:-.}
-
-# Find all files in the directory
-find "$DIRECTORY" -type f | while read -r file; do
-  echo "Formatting $file"
-  nvim --headless -c "edit $file" -c "lua vim.lsp.buf.format()" -c "write" -c "quit"
-done
