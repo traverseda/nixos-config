b6f26174eddee2234e24736a3529cefb8ed156ec
Alex Davies <traverse.da@gmail.com>
2025-02-17 12:43:18 -0400

    Added stubbs for simula when it works

--- nixos/vr-desktop.nix
+++ nixos/vr-desktop.nix
@@ -1,8 +1,43 @@
-{ config, pkgs, lib, ... }:
+{ inputs, outputs, config, pkgs, lib, ... }:
+let
+  # Fetch Simula source code from GitHub
+  # simulaSrc = pkgs.fetchFromGitHub {
+  #   owner = "traverseda";
+  #   repo = "Simula";
+  #   rev = "b232d1c672fbbda502ca4723e8f3fc5379dc7eec";
+  #   sha256 = "DmsUxSJZCqMjkvKx51NYomJ4Bm/fJ5h6rYSxkdpu1MM=";
+  #   fetchSubmodules = true;
+  # };
+
+  # # Build the Simula package
+  # simula = pkgs.callPackage "${simulaSrc}/Simula.nix" {
+  #   onNixOS = true;
+  #   devBuild = false;
+  #   profileBuild = false;
+  #   externalSrc = simulaSrc;
+  # };
+in
 {
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+
+  ];
   nixpkgs.overlays = [
     # (import (pkgs.fetchFromGitHub "https://github.com/SimulaVR/Simula.git"))
   ];
+  environment.systemPackages = [
+    pkgs.wlx-overlay-s
+    # simula
+  ];
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs outputs; };
+    backupFileExtension = ".bak";
+    users = {
+      traverseda = import ../home-manager/traverseda/vr.nix;
+    };
+  };
+
   services.wivrn = {
     enable = true;
     openFirewall = true;
