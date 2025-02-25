5f6fbf339ed5aa205625506e1f817c43067dad2a
Alex Davies <traverse.da@gmail.com>
2025-02-25 08:36:58 -0400

    fix: added duc package to nixos configuration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -104,6 +104,7 @@
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
     pkgs.exfat
+    pkgs.duc
     pkgs.glibcLocales
     pkgs.exfatprogs
     pkgs.mosh
