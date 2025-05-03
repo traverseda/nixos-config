04b264d467ecac6a1f7a416647a14137c577caa1
Alex Davies <traverse.da@gmail.com>
2025-05-03 11:23:31 -0300

    feat: remove custom power settings, handled by power-profiles-daemon

--- nixos/hardware/azrael.nix
+++ nixos/hardware/azrael.nix
@@ -33,10 +33,6 @@
     settings = {
       START_CHARGE_THRESH_BAT0 = 75;  # Start charging at 75%
       STOP_CHARGE_THRESH_BAT0 = 80;   # Stop charging at 80%
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
     };
   };
 
