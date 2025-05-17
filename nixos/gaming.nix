4cb000c63e64503936bee2ce6b6884fa3db8a33a
Alex Davies <traverse.da@gmail.com>
2025-05-17 09:29:35 -0300

    feat: enable additional Steam firewall ports and gamescope

--- nixos/gaming.nix
+++ nixos/gaming.nix
@@ -4,11 +4,13 @@
 
   programs.steam = {
     enable = true;
-    # remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    gamescopeSession.enable = true;
+
   };
   programs.steam.extraCompatPackages = [ pkgs.proton-ge-bin];
-
-  programs.gamescope.enable = true;
   programs.gamemode.enable = true;
 
   environment.systemPackages =[
